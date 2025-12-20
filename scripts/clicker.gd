0fbd392b4ff0a10952a590f1019b219daa31cf98
Ashkesh-D <ashlesh.deshmukh.sg@gmail.com>
2025-12-21 01:26:11 +0800

    Handling to indicate which shop part user is on addeD

--- scripts/clicker.gd
+++ scripts/clicker.gd
@@ -1,6 +1,8 @@
 extends Control
 @onready var among_us: Button = $AmongUs
-
+@onready var clicker_shop: Button = $Shop/ShopPanel/ClickerShop
+@onready var achievements: Button = $Shop/ShopPanel/Achievements
+@onready var user_shop: Button = $Shop/ShopPanel/UserShop
 
 signal among_pressed()
 func _on_among_us_button_down() -> void:
@@ -11,3 +13,24 @@ func _on_among_us_button_down() -> void:
 
 func _on_among_us_button_up() -> void:
 	among_us.scale = Vector2(1, 1)
+
+
+func _on_user_shop_pressed() -> void:
+	user_shop.toggle_mode = true
+	user_shop.button_pressed = true
+	clicker_shop.toggle_mode = false
+	achievements	.toggle_mode = false
+
+
+func _on_clicker_shop_pressed() -> void:
+	clicker_shop.toggle_mode = true
+	clicker_shop.button_pressed = true
+	achievements.toggle_mode = false
+	user_shop.toggle_mode = false
+
+
+func _on_achievements_pressed() -> void:
+	achievements.toggle_mode = true
+	achievements.button_pressed = true
+	user_shop.toggle_mode = false
+	clicker_shop.toggle_mode = false
