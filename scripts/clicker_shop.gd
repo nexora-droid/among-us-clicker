18b4866d58bfd70fffb510503f84a54ba3992735
Ashkesh-D <ashlesh.deshmukh.sg@gmail.com>
2025-12-21 00:07:17 +0800

    Shop begun

---
+++ scripts/clicker_shop.gd
@@ -0,0 +1,12 @@
+extends Button
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	toggle_mode = true
+	button_pressed = true
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
